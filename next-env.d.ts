dfcb6bc74af004c02025de4ee9514fc9ab7413ab
Developer <dev@example.com>
2026-05-06 11:30:51 +0500

    Cleanup build attempts and keep logo fix

--- next-env.d.ts
+++ next-env.d.ts
@@ -3,8 +3,3 @@
 
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/app/api-reference/config/typescript for more information.
-/// <reference types="next" />
-/// <reference types="next/image-types/global" />
-
-// NOTE: This file should not be edited
-// see https://nextjs.org/docs/app/api-reference/config/typescript for more information.
